ef701e621493c8c9cfb7490c4dd3f882035f65d2
Ujwal-Khairnar <ujwalkhairnar5@gmail.com>
2024-08-01 19:11:25 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://expenso-track-gex7.vercel.app/',
+    origin: 'https://expenso-track-gex7.vercel.app',
     // origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
