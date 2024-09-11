d22309b698d2cac9a5128db4955d384e6a0f9db4
Ujwal-Khairnar <110166392+Ujwal-Khairnar@users.noreply.github.com>
2024-09-12 02:28:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://ujwal-expenso-track.vercel.app',
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
